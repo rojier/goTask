69ff26e376edde7ae257409b97b9117f72e18fb6
isoftstone_luojie <568530214@qq.com>
2025-08-28 08:49:31 +0800

    密码加密

--- blog/service/userService.go
+++ blog/service/userService.go
@@ -3,6 +3,8 @@ package service
 import (
 	"blog/dao"
 	"errors"
+
+	"golang.org/x/crypto/bcrypt"
 )
 
 type UserService struct {
@@ -17,6 +19,12 @@ func (userService UserService) Regitser(user dao.User) error {
 	if len(users) > 0 {
 		return errors.New("该用户名已注册")
 	}
+	// 加密密码
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(user.PassWord), bcrypt.DefaultCost)
+	if err != nil {
+		return errors.New("加密密码错误")
+	}
+	user.PassWord = string(hashedPassword)
 	result := dao.DB.Create(&user)
 	if result.RowsAffected > 0 {
 		return nil
